e4dcda273e1076aaa57ca5a3943fae06ae74af77
Alex Mi <alexmi3.14@gmail.com>
2025-06-29 16:55:31 -0400

    Add the combined files

--- create_combined_files.py
+++ create_combined_files.py
@@ -32,16 +32,14 @@ def load_inherited_attributes(current_config, current_config_name='root'):
     return current_config
 
 
-machine_json_paths = { x.stem: x for x in Path('system/BBL').glob("**/*.json") if 'combined' not in x.stem}
-# filament_json_files = { x.stem: x for x in  Path('system/BBL/machine').glob("**/*.json")}
-# process_json_files = { x.stem: x for x in  Path('system/BBL/machine').glob("**/*.json")}
+json_paths = {x.stem: x for x in Path('system/BBL').glob("**/*.json") if 'combined' not in x.stem}
 
-for machine_json_path in machine_json_paths.values():
-    with open(machine_json_path) as machine_json_file:
-        logger.debug(f'Loading "{machine_json_path}" as json')
+for json_path in json_paths.values():
+    with open(json_path) as machine_json_file:
+        logger.debug(f'Loading "{json_path}" as json')
         try:
             machine_json_content = json.load(machine_json_file)
-            fully_loaded_config = load_inherited_attributes(machine_json_content, machine_json_path.stem)
+            fully_loaded_config = load_inherited_attributes(machine_json_content, json_path.stem)
             # pprint(fully_loaded_config)
 
             for k, v in fully_loaded_config.items():
@@ -54,16 +52,16 @@ for machine_json_path in machine_json_paths.values():
                         logger.error(f'Config object had a "_gcode" key with a value that was neither a string or list of strings')
 
             if 'inherits' in fully_loaded_config:
-                combined_file_name = machine_json_path.parent.joinpath('combined_' + machine_json_path.stem.lower().replace(' ', '_') +  machine_json_path.suffix)
+                combined_file_name = json_path.parent.joinpath('combined_' + json_path.stem.lower().replace(' ', '_') + json_path.suffix)
                 logger.debug(f'Combined file path will be: {combined_file_name}')
                 with open(combined_file_name, 'w') as combined_json_file:
                     logger.debug(f'Writing combined file to "{combined_file_name}"')
                     json.dump(fully_loaded_config, combined_json_file, indent=4, sort_keys=True)
                     logger.debug(f'Writing combined file to "{combined_file_name}" success')
             else:
-                logger.debug(f'Skipping output for parent machine: {machine_json_path.stem}')
+                logger.debug(f'Skipping output for parent machine: {json_path.stem}')
         except JSONDecodeError as e:
-            logger.error(f'Could not load "{machine_json_path}" as json: {e}')
+            logger.error(f'Could not load "{json_path}" as json: {e}')
             continue
 
 
